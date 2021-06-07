284c50f1d5f95758b081c48bede8a64f8e8ddda5
xreztech006 <xreztech006@gmail.com>
2021-06-07 12:28:53 -0500

    FoV Button Edits ('hide' bug - crawling)

---
+++ Assets/Scripts/AROnlyCameraManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AROnlyCameraManager : MonoBehaviour
+{
+	//public RenderTexture _active;
+	[Tooltip("Make sure 0 is the bot, and 1-3 are respective cameras")]
+	public Camera[] cameras;
+	private Camera cam, _cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Camera cam in cameras){
+			cam.targetTexture = null;
+		}
+		cam = _cam = cameras[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (_cam != cam)
+		{
+			_cam.enabled = true;
+			cam.enabled = false;
+			cam = _cam;
+		}
+	}
+
+	public void setCam(int num)
+	{
+		_cam = cameras[num];
+	}
+
+}
---
+++ Assets/Scripts/cameraCompassRose.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cameraCompassRose : MonoBehaviour
+{
+    private Text _text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = FindObjectOfType<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _text.text = System.String.Format("Rotation (X: {0}, Y: {1}, Z: {2}", Camera.main.transform.rotation.x, Camera.main.transform.rotation.y, Camera.main.transform.rotation.z);
+    }
+}
