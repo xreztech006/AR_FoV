316dab5ee71fae3de1117d4bb9feaeb7e3672a3e
xreztech006 <xreztech006@gmail.com>
2021-06-11 09:53:42 -0500

    Steady - 6/11/2021 - HDH

---
+++ Assets/Scripts/anchorCastHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anchorCastHandler : MonoBehaviour
+{
+	public GameObject vessel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 startsPunkt = Camera.main.transform.position;
+		Vector3 direction = startsPunkt - this.transform.position;
+		 // new Vector3(transform.position.x-xoffset, transform.position.y - ydecrement, transform.position.z - zoffset); 
+
+		RaycastHit hit;
+		// Does the ray intersect any objects excluding the player layer
+		if (Physics.Raycast(startsPunkt, direction, out hit, Mathf.Infinity, LayerMask.NameToLayer("botAnchor")))
+		{
+
+			vessel.transform.position = hit.point;
+		}
+		else vessel.transform.position = new Vector3(this.transform.position.x,
+													5.300132f,
+													this.transform.position.z);
+		vessel.transform.rotation = this.transform.rotation;
+
+	}
+}
